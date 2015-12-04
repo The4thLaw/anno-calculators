b496cad103ef169e6c9a6186a58ee2e9f7cd3dd2
The4thLaw <The4thLaw@users.noreply.github.com>
2015-12-04 20:39:36 +0100

    Heavy refactor  - Extract most JavaScript to common.js  - Properly namespace JavaScript variables and functions  - Clean markup to facilitate future works  - Implement save/load from cookies (fixes #3)

--- src/common.js
+++ src/common.js
@@ -1,13 +1,298 @@
 /*global console */
 /*global $ */
-
-// Initialise clickable headers
-$(function () {
-	'use strict';
+/*global prompt */
+/*global Cookies */
+var anno = {
+	computationTimer: null,
+	cookieName: null,
 	
-	$('h5, h6').click(function () {
-		var sectionContent = $('+ div', this);
-		sectionContent.slideToggle();
-	});
-});
+	getPopulationClass: function ($element) {
+		'use strict';
+		
+		return $($element.parents('section[data-pop-class]').get(0)).data('pop-class');
+	},
 
+	getPopulationCounts: function () {
+		'use strict';
+		
+		var counts = {};
+		$('input.pop_count').each(function () {
+			var $this = $(this),
+				count,
+				popClass,
+				popArr;
+			
+			count = $this.val();
+			count = count === '' ? 0 : parseInt(count, 10);
+			
+			popClass = anno.getPopulationClass($this);
+			
+			if (typeof (counts[popClass]) === 'undefined') {
+				counts[popClass] = [];
+			}
+			counts[popClass].push(count);
+		});
+		return counts;
+	},
+
+	getProductOrMaterialClass: function (element) {
+		'use strict';
+
+		var i,
+			classes;
+		
+		classes = $(element).attr('class').split(/ /);
+
+		for (i = 0; i < classes.length; i += 1) {
+			if (classes[i].match(/^(prod_|mat_)/)) {
+				return classes[i];
+			}
+		}
+	},
+
+	generateMarkup: function (game) {
+		'use strict';
+
+		$('[data-sum-of]').each(function () {
+			$(this).addClass($(this).data('sum-of'));
+		});
+		
+		// Add the summary class where relevant
+		$('#summaries .product, #summaries .material').addClass('summary');
+
+		$('.product, .material').each(function () {
+			$(this).append('<img src="img/' + game + '/' + anno.getProductOrMaterialClass(this) + '.png" >');
+
+			$(this).append('<span class="count--rounded">0</span><span class="count">0</span>');
+			if (!$(this).is('.summary')) {
+				$(this).append(' @ <input class="mdl-textfield__input efficiency" type="number" value="100">%');
+			}
+		});
+
+		// Now, generate IDs for efficiencies
+		$('.chain input').each(function () {
+			var $this = $(this),
+				id = $this.attr('id'),
+				section,
+				sectionId,
+				chain,
+				productClass;
+
+			if ((typeof (id) === 'undefined') || id === '') {
+				// Gen from section, first product and current product
+				section = $this.parents('section').get(0);
+				sectionId = $(section).attr('id');
+				chain = $this.parents('.chain').get(0);
+				productClass = anno.getProductOrMaterialClass($('> .product', chain));
+				id = 'eff_' + sectionId + '_' + productClass;
+				if ($($this.parent()).is('.material')) {
+					id += '_' + anno.getProductOrMaterialClass($this.parent());
+				}
+				$this.attr('id', id);
+			}
+		});
+	},
+
+	getEfficiency: function (scope) {
+		'use strict';
+
+		var efficiency = $('input.efficiency', scope).val();
+		return efficiency === '' ? 1 : parseInt(efficiency, 10) / 100;
+	},
+
+	setCount: function (element, count) {
+		'use strict';
+
+		$(element).data('raw-count', count);
+		var roundedCount = Math.ceil(count);
+		count = Math.round(count * 100) / 100;
+		$('.count', element).html(count);
+		$('.count--rounded', element).html(roundedCount);
+
+		if (count === 0) {
+			$(element).addClass('no-need');
+		} else {
+			$(element).removeClass('no-need');
+		}
+	},
+
+	computeMaterial: function (element) {
+		'use strict';
+
+		var factoryName = $(element).data('factory'),
+			factory = $('.' + factoryName, $(element).parents('.chain').get(0)),
+			efficiency = anno.getEfficiency(element),
+			count = parseFloat(factory.data('raw-count')) * parseFloat($(element).data('ratio')) / efficiency;
+		anno.setCount(element, count);
+	},
+
+	doCompute: function () {
+		'use strict';
+
+		var overallPopulationCounts = anno.getPopulationCounts();
+
+		$('.product:not(.summary)').each(function () {
+			var $this = $(this),
+				efficiency,
+				supports,
+				count = 0,
+				i,
+				specificPopulationCounts;
+			
+			efficiency = anno.getEfficiency(this);
+			supports = $this.data('supports').split(/,/);
+			specificPopulationCounts = overallPopulationCounts[anno.getPopulationClass($this)];
+			
+			for (i = 0; i < specificPopulationCounts.length; i += 1) {
+				if (specificPopulationCounts[i] > 0 && supports[i] > 0) {
+					count += specificPopulationCounts[i] / supports[i] / efficiency;
+				}
+			}
+			anno.setCount(this, count);
+		});
+
+		$('.material.chain--level-1').each(function () {
+			anno.computeMaterial(this);
+		});
+		$('.material.chain--level-2').each(function () {
+			anno.computeMaterial(this);
+		});
+
+		$('[data-sum-of]').each(function () {
+			var elements = $('.' + $(this).data('sum-of') + ':not(.summary)'),
+				sum = 0;
+			elements.each(function () {
+				sum += parseFloat($(this).data('raw-count'));
+			});
+			anno.setCount(this, sum);
+		});
+	},
+
+	compute: function () {
+		'use strict';
+
+		clearTimeout(anno.computationTimer);
+		anno.computationTimer = setTimeout(anno.doCompute, 100);
+	},
+
+	getSaveData: function () {
+		'use strict';
+
+		var efficiencies = {};
+
+		$('.efficiency').each(function () {
+			var $this = $(this);
+			if ($this.val() !== '100') {
+				efficiencies[$this.attr('id')] = $this.val();
+			}
+		});
+
+		return {
+			population: anno.getPopulationCounts(),
+			efficiencies: efficiencies
+		};
+	},
+
+	saveCity: function () {
+		'use strict';
+
+		var cityName, data = {};
+
+		cityName = prompt('City or game name:');
+
+		// Restore previous data
+		data = Cookies.getJSON(anno.cookieName);
+		if (typeof (data) === 'undefined') {
+			data = {};
+		}
+		// Include new one
+		data[cityName] = anno.getSaveData();
+
+		Cookies.set(anno.cookieName, data, { expires: 365 });
+		
+		// Refresh save list
+		anno.fillSaveList();
+	},
+
+	loadCity: function () {
+		'use strict';
+		
+		var fullData, data;
+		
+		fullData = Cookies.getJSON(anno.cookieName);
+		data = fullData[$('#save-list').val()];
+		
+		// Load population
+		$.each(data.population, function (popClass, values) {
+			$('#population section[data-pop-class="' + popClass + '"] input.pop_count').each(function (i) {
+				if (values[i] > 0) {
+					$(this).val(values[i]);
+					// Fix MDL
+					$(this).parent().addClass('is-dirty');
+				}
+			});
+		});
+		
+		// Load efficiencies
+		// Reset them all, first
+		$('.efficiency').val('100');
+		$.each(data.efficiencies, function (id, value) {
+			$('#' + id).val(value);
+		});
+		
+		anno.compute();
+	},
+	
+	fillSaveList: function () {
+		'use strict';
+		
+		var data, $select;
+		
+		$select = $('#save-list');
+		$select.html('');
+		
+		data = Cookies.getJSON(anno.cookieName);
+		$.each(data, function (key) {
+			$select.append('<option value="' + key + '">' + key + '</option>');
+		});
+	},
+	
+	init: function (game) {
+		'use strict';
+		
+		anno.cookieName = 'anno' + game;
+		anno.generateMarkup(game);
+		
+		// Initialise clickable headers
+		$('h5, h6').click(function () {
+			var sectionContent = $('+ div', this);
+			sectionContent.slideToggle();
+		});
+		
+		// Initialise load/save buttons
+		$('#btn-save').click(anno.saveCity);
+		$('#btn-load').click(anno.loadCity);
+		
+		// Bind change handlers
+		$('input.pop_count, input.efficiency').change(anno.compute);
+		$('input.pop_count, input.efficiency').keyup(anno.compute);
+		$('input.pop_count, input.efficiency').click(anno.compute);
+
+		// Handle option to round counts
+		$('.count--rounded').hide();
+		$('#opt-round-counts').change(function () {
+			if ($(this).is(':checked')) {
+				$('.count--rounded').show();
+				$('.count').hide();
+			} else {
+				$('.count--rounded').hide();
+				$('.count').show();
+			}
+		});
+		$('#opt-round-counts').change();
+		
+		// Initialise save list
+		anno.fillSaveList();
+	}
+
+};
